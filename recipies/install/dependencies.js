aaff6e489ac1a127550ca54f17638922e03bb434
Javier Lopez de Ancos <javierlopezdeancos@gmail.com>
2015-04-14 11:31:11 +0200

    add task to fix install theme dependencie with any bad key

--- recipies/install/dependencies.js
+++ recipies/install/dependencies.js
@@ -1,22 +1,26 @@
-/*****************************************/
-/**  RECIPIE                            **/
-/**  Name:   Install Dependencies       **/
-/**  Tasks:  install:depdendencies      **/
-/**          browserSyncSass            **/
-/**          browserSync                **/
-/**          watch                      **/
-/**  Output: install:dependencies:bower **/
-/**          install:dependencies:npm   **/
-/**          install:dependencies:theme **/
-/*****************************************/
+/*********************************************************/
+/**  RECIPIE                            								**/
+/**  Name:   Install Dependencies       								**/
+/**  Tasks:  install:depdendencies      								**/
+/**          browserSyncSass            								**/
+/**          browserSync                								**/
+/**          watch                      								**/
+/**  Output: install:dependencies:bower 								**/
+/**          install:dependencies:npm   								**/
+/**          install:dependencies:theme 								**/
+/**          clean:dependencies:theme   								**/
+/**          clean:dependencies:themeinstaledbydefault  **/
+/** 				 rename:dependencies:theme 									**/
+/*********************************************************/
 
 'use strict';
 
 var gulp = gulp || require('gulp'),
   shell = require('gulp-shell'),
+  clean = require('gulp-clean'),
   higgsboson = higgsboson || require('../../../../higgsboson.json'),
   theme = require('../../../../theme.json'),
-  bower = require('../../../../bower.json');;
+  bower = require('../../../../bower.json');
 
 gulp.task('install:dependencies:bower', shell.task([
   'bower install'
@@ -25,6 +29,21 @@ gulp.task('install:dependencies:bower', shell.task([
 gulp.task('install:dependencies:npm', shell.task([
   'npm install',
 ]));
+
+gulp.task('clean:dependencies:theme', function () {
+  return gulp.src( higgsboson.path.dependencies + '/' + theme.key, {read: false})
+    .pipe(clean());
+});
+
+gulp.task('clean:dependencies:themeinstaledbydefault', function () {
+  return gulp.src( higgsboson.path.dependencies + '/' + theme.key + '-**', {read: false})
+    .pipe(clean());
+});
+
+gulp.task('rename:dependencies:theme', function () {
+  return gulp.src( higgsboson.path.dependencies + '/' + theme.key + '-**', {read: false})
+    .pipe(gulp.dest(higgsboson.path.dependencies + '/' + theme.key));
+});
 
 gulp.task('install:dependencies:theme', shell.task([
   'bower install ' + theme.url,
--- tasks/install.js
+++ tasks/install.js
@@ -3,9 +3,10 @@
 var gulp = gulp || require('gulp');
 
 gulp.task('install', [
+	'clean:dependencies:theme',
 	'install:dependencies:npm',
 	'install:dependencies:bower',
-	'install:dependencies:theme'
+	'install:dependencies:theme',
+	'rename:dependencies:theme',
+	'clean:dependencies:themeinstaledbydefault'
 ]);
-
-
