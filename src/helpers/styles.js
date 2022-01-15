3bc5e035cfedf2afc9865ede0de12350c260b846
Javier López de Ancos <javierlopezdeancos@outlook.com>
2022-01-15 20:13:11 +0100

    refactor: apply underscore template to box

---
+++ src/helpers/styles.js
@@ -0,0 +1,33 @@
+const headerStyles = ["margin-bottom: 20px", "margin-top: 0"];
+
+const headerWithSubtitleStyles = ["margin-bottom: 0", "margin-top: 0"];
+
+const subHeaderStyles = [
+  "margin-top: 0",
+  "margin-bottom: 20px",
+  "margin-left: 20px;",
+  "color: #565656",
+];
+
+const subHeaderWithChildStyles = [
+  "margin-top: 0",
+  "margin-bottom: 0",
+  "margin-left: 20px;",
+  "color: #565656",
+];
+
+const subHeaderChildStyles = [
+  "margin-top: 0",
+  "margin-bottom: 20px",
+  "margin-left: 40px;",
+  "color: #707070",
+];
+
+const subHeaderChildWithParentWithSiblingStyles = [
+  "margin-top: 0",
+  "margin-bottom: 0",
+  "margin-left: 40px;",
+  "color: #707070",
+];
+
+const articleStyles = ["width: 100%", "margin-bottom: 30px"];
