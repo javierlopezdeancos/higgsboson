f3f4a443a33b7d454968aa9592cf5b6be843d164
Javier López de Ancos <javierlopezdeancos@outlook.com>
2022-01-16 13:14:03 +0100

    refactor: add correct focus to checkbox

---
+++ src/components/checkbox/checkbox.metadata.js
@@ -0,0 +1,18 @@
+const data = {
+  title: "Checkbox",
+  default: [
+    {
+      title: "checkbox",
+      subtitle: "label",
+      styles: {
+        header: headerWithSubtitleStyles,
+        article: articleStyles,
+        subHeaderH4: subHeaderStyles,
+      },
+      classes: {
+        wrapper: ["hggs-checkbox"],
+        label: ["hggs-label"],
+      },
+    },
+  ],
+};
