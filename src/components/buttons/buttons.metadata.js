60d05f8450bbcee014ab3cdf9d05805e68354c1b
Javier López de Ancos <javierlopezdeancos@outlook.com>
2022-02-02 20:42:43 +0100

    refactor: extract icon classes to a common file

--- src/components/buttons/buttons.metadata.js
+++ src/components/buttons/buttons.metadata.js
@@ -1,13 +1,3 @@
-const iconsClasses = [
-  ["hggs-icon", "hggs-icon--edit"],
-  ["hggs-icon", "hggs-icon--hamburger-menu"],
-  ["hggs-icon", "hggs-icon--log-out"],
-  ["hggs-icon", "hggs-icon--question"],
-  ["hggs-icon", "hggs-icon--user"],
-  ["hggs-icon", "hggs-icon--close"],
-  ["hggs-icon", "hggs-icon--plus"],
-];
-
 const headerWithSubtitleQuinaryStyles = ["margin-bottom: 0", "color: white"];
 
 const headerQuinaryStyles = ["margin-bottom: 20px", "color: white"];
---
+++ src/helpers/classes.js
@@ -0,0 +1,9 @@
+const iconsClasses = [
+  ["hggs-icon", "hggs-icon--edit"],
+  ["hggs-icon", "hggs-icon--hamburger-menu"],
+  ["hggs-icon", "hggs-icon--log-out"],
+  ["hggs-icon", "hggs-icon--question"],
+  ["hggs-icon", "hggs-icon--user"],
+  ["hggs-icon", "hggs-icon--close"],
+  ["hggs-icon", "hggs-icon--plus"],
+];
