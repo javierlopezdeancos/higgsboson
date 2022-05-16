c70db371de888184761b94585be62cc3abb418af
Javier Lopez de Ancos <javierlopezdeancos@gmail.com>
2022-05-16 19:40:00 +0200

    refactor: add underscore to title component

---
+++ src/components/titles/titles.metadata.js
@@ -0,0 +1,71 @@
+data = {
+  title: "Title",
+  default: [
+    {
+      title: "h1",
+      styles: {
+        header: headerStyles,
+        article: articleStyles,
+      },
+      classes: {
+        title: ["hggs-h1"],
+      },
+      label: "This is an h1 title",
+    },
+    {
+      title: "h2",
+      styles: {
+        header: headerStyles,
+        article: articleStyles,
+      },
+      classes: {
+        title: ["hggs-h2"],
+      },
+      label: "This is an h2 title",
+    },
+    {
+      title: "h3",
+      styles: {
+        header: headerStyles,
+        article: articleStyles,
+      },
+      classes: {
+        title: ["hggs-h3"],
+      },
+      label: "This is an h3 title",
+    },
+    {
+      title: "h4",
+      styles: {
+        header: headerStyles,
+        article: articleStyles,
+      },
+      classes: {
+        title: ["hggs-h4"],
+      },
+      label: "This is an h4 title",
+    },
+    {
+      title: "h5",
+      styles: {
+        header: headerStyles,
+        article: articleStyles,
+      },
+      classes: {
+        title: ["hggs-h5"],
+      },
+      label: "This is an h5 title",
+    },
+    {
+      title: "h6",
+      styles: {
+        header: headerStyles,
+        article: articleStyles,
+      },
+      classes: {
+        title: ["hggs-h6"],
+      },
+      label: "This is an h6 title",
+    },
+  ],
+};
