87de794793f790737f6825a369d2110b3f1dbd83
Javier López de Ancos <javierlopezdeancos@outlook.com>
2022-06-08 19:08:40 +0200

    Add underscore template in grid component (#70)
    * refactor: add underscore template to grid component

---
+++ src/components/grid/grid.md
@@ -0,0 +1,70 @@
+# Grid
+
+## Root component class name
+
+`hggs-grid`
+
+## Other elements component class names
+
+`hggs-cell`
+
+## HTML Structure
+
+### Default
+
+#### Basic HTML structure
+
+```html
+<div class="hggs-grid">
+  <div class="hggs-cell hggs-cell--4of12"></div>
+</div>
+```
+
+#### Basic HTML structure with `center` modifier
+
+```html
+<div class="hggs-grid hggs-grid--center">
+  <div class="hggs-cell hggs-cell--6of12"></div>
+  <div class="hggs-cell hggs-cell--2of12"></div>
+</div>
+```
+
+#### Basic HTML structure with `right` modifier
+
+```html
+<div class="hggs-grid hggs-grid--right">
+  <div class="hggs-cell hggs-cell--6of12"></div>
+  <div class="hggs-cell hggs-cell--2of12"></div>
+</div>
+```
+
+#### Basic HTML structure with `left` modifier
+
+```html
+<div class="hggs-grid hggs-grid--left">
+  <div class="hggs-cell hggs-cell--6of12"></div>
+  <div class="hggs-cell hggs-cell--2of12"></div>
+</div>
+```
+
+#### Basic HTML structure with `between` modifier
+
+```html
+<div class="hggs-grid hggs-grid--between">
+  <div class="hggs-cell hggs-cell--6of12"></div>
+  <div class="hggs-cell hggs-cell--2of12"></div>
+</div>
+```
+
+#### Basic HTML structure with so many cells
+
+```html
+<div class="hggs-grid">
+  <div class="hggs-cell hggs-cell--6of12"></div>
+  <div class="hggs-cell hggs-cell--2of12"></div>
+  <div class="hggs-cell hggs-cell--4of12"></div>
+  <div class="hggs-cell hggs-cell--4of12"></div>
+  <div class="hggs-cell hggs-cell--3of12"></div>
+  <div class="hggs-cell hggs-cell--5of12"></div>
+</div>
+```
---
+++ src/components/grid/grid.metadata.js
@@ -0,0 +1,148 @@
+const sectionStyles = [
+  "width: 100%",
+  "padding: 30px",
+  "margin-bottom: 20px",
+  "background: #fbfbfb",
+];
+
+const data = {
+  title: "Grid",
+  default: [
+    {
+      title: "grid",
+      subtitle: "cell --4of12",
+      styles: {
+        header: headerWithSubtitleStyles,
+        article: articleStyles,
+        subHeaderH4: subHeaderStyles,
+        section: sectionStyles,
+      },
+      classes: {
+        grid: ["hggs-grid"],
+        cell: ["hggs-cell", "hggs-cell--4of12"],
+      },
+    },
+    {
+      title: "grid --center",
+      subtitle: "cell --6of12",
+      styles: {
+        header: headerWithSubtitleStyles,
+        article: articleStyles,
+        subHeaderH4: subHeaderStyles,
+        section: sectionStyles,
+      },
+      classes: {
+        grid: ["hggs-grid hggs-grid--center"],
+        cell: ["hggs-cell", "hggs-cell--6of12"],
+      },
+    },
+    {
+      title: "grid --center",
+      subtitle: "cell --4of12 + cell --2of12",
+      styles: {
+        header: headerWithSubtitleStyles,
+        article: articleStyles,
+        subHeaderH4: subHeaderStyles,
+        section: sectionStyles,
+      },
+      classes: {
+        grid: ["hggs-grid hggs-grid--center"],
+        cells: [
+          ["hggs-cell", "hggs-cell--4of12"],
+          ["hggs-cell", "hggs-cell--2of12"],
+        ],
+      },
+    },
+    {
+      title: "grid --center",
+      subtitle: "cell --4of12 + cell --2of12 + cell --4of12",
+      styles: {
+        header: headerWithSubtitleStyles,
+        article: articleStyles,
+        subHeaderH4: subHeaderStyles,
+        section: sectionStyles,
+      },
+      classes: {
+        grid: ["hggs-grid hggs-grid--center"],
+        cells: [
+          ["hggs-cell", "hggs-cell--4of12"],
+          ["hggs-cell", "hggs-cell--2of12"],
+          ["hggs-cell", "hggs-cell--4of12"],
+        ],
+      },
+    },
+    {
+      title: "grid --right",
+      subtitle: "cell --4of12 + cell --2of12",
+      styles: {
+        header: headerWithSubtitleStyles,
+        article: articleStyles,
+        subHeaderH4: subHeaderStyles,
+        section: sectionStyles,
+      },
+      classes: {
+        grid: ["hggs-grid hggs-grid--right"],
+        cells: [
+          ["hggs-cell", "hggs-cell--4of12"],
+          ["hggs-cell", "hggs-cell--2of12"],
+        ],
+      },
+    },
+    {
+      title: "grid --left",
+      subtitle: "cell --4of12 + cell --2of12",
+      styles: {
+        header: headerWithSubtitleStyles,
+        article: articleStyles,
+        subHeaderH4: subHeaderStyles,
+        section: sectionStyles,
+      },
+      classes: {
+        grid: ["hggs-grid hggs-grid--left"],
+        cells: [
+          ["hggs-cell", "hggs-cell--4of12"],
+          ["hggs-cell", "hggs-cell--2of12"],
+        ],
+      },
+    },
+    {
+      title: "grid --between",
+      subtitle: "cell --4of12 + cell --2of12",
+      styles: {
+        header: headerWithSubtitleStyles,
+        article: articleStyles,
+        subHeaderH4: subHeaderStyles,
+        section: sectionStyles,
+      },
+      classes: {
+        grid: ["hggs-grid hggs-grid--between"],
+        cells: [
+          ["hggs-cell", "hggs-cell--4of12"],
+          ["hggs-cell", "hggs-cell--2of12"],
+        ],
+      },
+    },
+    {
+      title: "grid",
+      subtitle:
+        "cell --6of12 + cell --2of12 + cell --4of12 + cell --4of12 + cell --3of12 + cell --5of12 ",
+      styles: {
+        header: headerWithSubtitleStyles,
+        article: articleStyles,
+        subHeaderH4: subHeaderStyles,
+        section: sectionStyles,
+      },
+      classes: {
+        grid: ["hggs-grid"],
+        cells: [
+          ["hggs-cell", "hggs-cell--6of12"],
+          ["hggs-cell", "hggs-cell--2of12"],
+          ["hggs-cell", "hggs-cell--4of12"],
+          ["hggs-cell", "hggs-cell--4of12"],
+          ["hggs-cell", "hggs-cell--3of12"],
+          ["hggs-cell", "hggs-cell--5of12"],
+        ],
+      },
+    },
+  ],
+};
