28b1e85dd4b977fb7e10aaa82e7490d8c8cefd5d
Javier Lopez de Ancos <javierlopezdeancos@gmail.com>
2022-06-29 14:17:51 +0200

    chore: update dependencies and improve config postcss file

--- postcss.config.js
+++ postcss.config.js
@@ -1,27 +1,35 @@
-const postcssPresetEnv = require("postcss-preset-env");
+const presetEnv = require("postcss-preset-env");
 const autoprefixer = require("autoprefixer");
-const cssnano = require("cssnano");
+const nesting = require("postcss-nesting");
 const atImport = require("postcss-import");
-const simpleExtend = require("postcss-extend");
-const postcssNesting = require("postcss-nesting");
+const extend = require("postcss-extend");
 const stylelint = require("stylelint");
 const copyAssets = require("postcss-copy-assets");
-const postCssCsso = require("postcss-csso");
 
 module.exports = (ctx) => ({
   map: ctx.options.map,
   parser: ctx.options.parser,
-  plugins: [
-    postcssPresetEnv({ stage: 4 }),
-    atImport({
-      from: "src/index.css",
-    }),
-    postcssNesting(),
-    simpleExtend(),
-    stylelint(),
-    autoprefixer(),
-    cssnano({ preset: "default" }),
-    postCssCsso({ restructure: false }),
-    copyAssets({ base: "dist" }),
-  ],
+  plugins:
+    process.env.NODE_ENV === "production"
+      ? [
+          presetEnv({
+            stage: 0,
+          }),
+          atImport(),
+          copyAssets({ base: "dist" }),
+          nesting(),
+          extend(),
+          stylelint(),
+          autoprefixer(),
+          // minify(),
+        ]
+      : [
+          presetEnv({ stage: 0 }),
+          atImport(),
+          copyAssets({ base: "dist" }),
+          nesting(),
+          extend(),
+          stylelint(),
+          autoprefixer(),
+        ],
 });
