dbac6ffaf99defe651f43ce27fc09d36a02a51c2
Javier Lopez de Ancos <javier.lopezdeancos@wayflyer.com>
2022-11-21 20:21:01 +0100

    add hggs-switch component working

---
+++ src/components/switch/switch.metadata.js
@@ -0,0 +1,23 @@
+const data = {
+  title: "Switch",
+  default: [
+    {
+      title: "switch",
+      subtitle: "label",
+      styles: {
+        header: headerWithSubtitleStyles,
+        subHeader: subHeaderStyles,
+        article: [...articleStyles],
+      },
+      classes: {
+        switch: ["hggs-switch"],
+      },
+      ids: {
+        input: "switch-1",
+      },
+      fors: {
+        label: "switch-1",
+      },
+    },
+  ],
+};
