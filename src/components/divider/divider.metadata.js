de161a3e7b1b1169f500529c3ab9b15c89d66278
Javier Lopez de Ancos <javier.lopezdeancos@wayflyer.com>
2022-11-24 19:21:16 +0100

    feat: set variables to caramel and captain america themes

---
+++ src/components/divider/divider.metadata.js
@@ -0,0 +1,15 @@
+const data = {
+  title: "Divider",
+  default: [
+    {
+      title: "divider",
+      styles: {
+        header: headerStyles,
+        article: articleStyles,
+      },
+      classes: {
+        divider: ["hggs-divider"],
+      },
+    },
+  ],
+};
