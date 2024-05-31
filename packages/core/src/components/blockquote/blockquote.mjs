e29ae791a0637a70d3fa9a6515556c94c1c19447
Javier López de Ancos <javierlopezdeancos@gmail.com>
2024-05-31 17:46:25 +0200

    feat: add blockquote component (#163)

---
+++ packages/core/src/components/blockquote/blockquote.mjs
@@ -0,0 +1,25 @@
+const data = {
+  title: "Blockquote",
+  default: [
+    {
+      titles: {
+        blockquote: "hggs-blockquote",
+        text: "hggs-text",
+      },
+      classes: {
+        blockquote: ["hggs-blockquote"],
+        text: ["hggs-text"],
+      },
+      styles: {
+        header: headerStyles,
+        article: articleStyles,
+        titles: {
+          blockquote: titleStyles,
+          text: titleIndentation1xStyles,
+        },
+      },
+    },
+  ],
+};
+
+export default data;
